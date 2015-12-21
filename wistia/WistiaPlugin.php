14a9452cb0a6298da54a4bf95cad73694ee2eea8
Michael Leigeber <michael@leigeber.com>
2015-12-21 17:02:18 -0600

    Initial scaffolding commit

---
+++ wistia/WistiaPlugin.php
@@ -0,0 +1,69 @@
+<?php
+namespace Craft;
+
+class WistiaPlugin extends BasePlugin
+{
+	public function getName()
+	{
+		return 'Wistia';
+	}
+
+	public function getDescription()
+	{
+		return 'Manage videos and output data using the Wistia API.';
+	}
+
+	public function getVersion()
+	{
+		return '0.1.0';
+	}
+
+	public function getDeveloper()
+	{
+		return 'Caddis';
+	}
+
+	public function getDeveloperUrl()
+	{
+		return 'https://www.caddis.co';
+	}
+
+	public function getDocumentationUrl()
+	{
+		return 'https://www.caddis.co/software/craft/wistia';
+	}
+
+	public function getSchemaVersion()
+	{
+		return '1.0.0';
+	}
+
+	public function hasCpSection()
+	{
+		return true;
+	}
+
+	protected function defineSettings()
+	{
+		return array(
+			'apiKey' => array(
+				AttributeType::String
+			),
+			'cacheDuration' => array(
+				AttributeType::Number,
+				24
+			),
+			'thumbnailPath' => array(
+				AttributeType::String,
+				'/images/videos/'
+			)
+		);
+	}
+
+	public function getSettingsHtml()
+	{
+		return craft()->templates->render('wistia/plugin/settings', array(
+			'settings' => $this->getSettings()
+		));
+	}
+}
---
+++ wistia/fieldtypes/Wistia_VideosFieldType.php
@@ -0,0 +1,84 @@
+<?php
+namespace Craft;
+
+class Wistia_VideosFieldType extends BaseOptionsFieldType
+{
+	/**
+	 * Fieldtype name
+	 *
+	 * @return string
+	 */
+	public function getName()
+	{
+		return Craft::t('Wistia Videos');
+	}
+
+	/**
+	 * Define database column
+	 *
+	 * @return AttributeType::String
+	 */
+	public function defineContentAttribute()
+	{
+		return array(AttributeType::String);
+	}
+
+	/**
+	 * Returns the label for the Options setting.
+	 *
+	 * @access protected
+	 * @return string
+	 */
+	protected function getOptionsSettingsLabel()
+	{
+		return Craft::t('Wistia Video Options');
+	}
+
+	/**
+	 * Defines the settings
+	 *
+	 * @access protected
+	 * @return array
+	 */
+	protected function defineSettings()
+	{
+		return array(
+			'projects' => array(
+				AttributeType::Mixed,
+				'default' => array(
+					'' => Craft::t('None')
+				)
+			),
+			'min' => array(
+				AttributeType::Number,
+				'default' => 0
+			),
+			'max' => array(
+				AttributeType::Number
+			)
+		);
+	}
+
+	/**
+	 * Display the field settings
+	 *
+	 * @return string
+	 */
+	public function getSettingsHtml()
+	{
+		return craft()->templates->render('wistia/fieldtype/settings', array(
+			'settings' => $this->getSettings()
+		));
+	}
+
+	/**
+	 * Sanitize the settings
+	 *
+	 * @param array $settings
+	 * @return array
+	 */
+	public function prepSettings($settings)
+	{
+		return $settings;
+	}
+}
