fbd54799f40c72c43645e2777d134532d12f3328
Ryan Payne <ryan@caddis.co>
2016-02-01 11:21:23 -0600

    Add video table

---
+++ records/Wistia_Video.php
@@ -0,0 +1,47 @@
+<?php
+namespace Craft;
+
+class Wistia_Video extends BaseRecord
+{
+	public function getTableName()
+	{
+		return 'wistia_video';
+	}
+
+	protected function defineAttributes()
+	{
+		return [
+			'id' => [
+				AttributeType::Number,
+				'column' => ColumnType::PK
+			],
+			'siteId' => AttributeType::Bool,
+			'sectionId' => AttributeType::Number,
+			'entryId' => AttributeType::Number,
+			'fieldId' => AttributeType::Number,
+			'type' => [
+				AttributeType::Enum,
+				'values' => 'single, channel, structure'
+			],
+			'wistiaId' => AttributeType::Number,
+			'hashedId' => AttributeType::Number,
+			'memberId' => AttributeType::Number,
+			'position' => AttributeType::Bool
+		];
+	}
+
+	public function defineIndexes()
+	{
+		return [
+			[
+				'columns' => 'type'
+			],
+			[
+				'columns' => 'wistiaId'
+			],
+			[
+				'columns' => 'hashedId'
+			]
+		];
+	}
+}
