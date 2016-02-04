f54633dfdd9b0879322f15f6d0d70c544406bddb
Ryan Payne <ryan@caddis.co>
2016-02-04 13:09:35 -0600

    Update Wistia variable

--- variables/WistiaVariable.php
+++ variables/WistiaVariable.php
@@ -5,6 +5,14 @@ class WistiaVariable
 {
 	public function videos($wistiaIds, $params = [])
 	{
-		return craft()->wistia_videos->getVideosByHashedIds($wistiaIds, $params);
+		$videos = [];
+
+		$wistiaIds = json_decode($wistiaIds);
+
+		foreach ($wistiaIds as $wistiaId) {
+			$videos[] = craft()->wistia_videos->getVideoByHashedId($wistiaId, $params);
+		}
+
+		return $videos;
 	}
 }
