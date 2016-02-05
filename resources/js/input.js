fec1a66ce004d151ab5c209e765ebe3664ecaa4d
Ryan Payne <ryan@caddis.co>
2016-02-05 14:34:07 -0600

    Add Garnish functions

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,28 +1,25 @@
-var $modal = $('.js-wistia-modal'),
-	$element = $('.js-wistia-element'),
-	$elements = $('.js-wistia-elements'),
-	$elementRow = $('.js-wistia-element-row'),
-	$submit = $('.js-wistia-submit'),
+/**
+ * Define global vars (yucky)
+ */
+var $element = $('.js-element'),
+	$elements = $('.js-elements'),
+	$elementRow = $('.js-element-row'),
+	$submit = $('.js-submit'),
 	$garnishModal,
+	$sorter,
 	isDisabled = 'disabled',
 	isSelected = 'sel',
 	isRemovable = 'removable';
 
 /**
- * Garnish modal
+ * Update selectable elements in the modal
  */
-$('.js-wistia-add-video').on('click', function() {
+function updateSelections() {
 	var selectedElements = [];
 
-	$('.js-wistia-element.removable').each(function() {
+	$('.js-element.removable').each(function() {
 		selectedElements.push($(this).data('id'));
 	});
-
-	if (! $garnishModal) {
-		$garnishModal = new Garnish.Modal($modal);
-	} else {
-		$garnishModal.show();
-	}
 
 	$elementRow.each(function(e, el) {
 		var $el = $(el);
@@ -35,60 +32,103 @@ $('.js-wistia-add-video').on('click', function() {
 			}
 		});
 	});
-});
-
-$('.js-wistia-close-modal').on('click', function() {
-	$garnishModal.hide();
-});
+}
 
 /**
- * Remove selected item
+ * Garnish modal
  */
-$elements.on('click', '.js-wistia-remove-video', function() {
-	$(this).parent().remove();
-});
+function modal() {
+	$('.js-open-modal').on('click', function() {
+		if (! $(this).hasClass(isDisabled)) {
+			updateSelections();
 
-/**
- * Select video row
- */
-$elementRow.on('click', function() {
-	var $this = $(this);
+			if (! $garnishModal) {
+				$garnishModal = new Garnish.Modal($('.js-modal'));
 
-	if (! $this.hasClass(isDisabled)) {
-		$this.toggleClass(isSelected);
-	}
+				selectElement();
+				submitSelections();
+			} else {
+				$garnishModal.show();
+			}
+		}
+	});
 
-	if ($('.js-wistia-element-row.sel').length > 0) {
-		$submit.removeClass(isDisabled);
-	} else {
-		$submit.addClass(isDisabled);
-	}
-});
-
-/**
- * Submit video selections
- */
-$submit.on('click', function() {
-	var $this = $(this),
-		$selections = $('.js-wistia-element-row.sel');
-
-	if (! $this.hasClass(isDisabled)) {
-		$selections.each(function(e, el) {
-			var $el = $(el);
-
-			$elements.append($el.find($element)
-				.clone()
-				.addClass(isRemovable)
-				.prepend('<input name="fields[' + $el.data('name') + '][]" type="hidden" value="' + $el.data('id') + '">' +
-					'<a class="delete icon js-wistia-remove-video"></a>'
-				)
-			);
-		});
-
-		$selections.removeClass(isSelected).addClass(isDisabled);
-
-		$this.addClass(isDisabled);
-
+	$('.js-close-modal').on('click', function() {
 		$garnishModal.hide();
-	}
-});
+	});
+}
+
+/**
+ * Remove selected element
+ */
+function removeElement() {
+	$elements.on('click', '.js-remove-element', function() {
+		$parent = $(this).parent();
+
+		$parent.remove();
+
+		$sorter.removeItems($parent);
+	});
+}
+
+/**
+ * Select element row
+ */
+function selectElement() {
+	var rowSelect = new Garnish.Select($('.js-element-body'), $elementRow.not('.disabled'), {
+			multi: true,
+			onSelectionChange: function() {
+				if ($('.js-element-row.sel').length > 0) {
+					$submit.removeClass(isDisabled);
+				} else {
+					$submit.addClass(isDisabled);
+				}
+			}
+		});
+}
+
+/**
+ * Submit element row selections
+ */
+function submitSelections() {
+	$submit.on('click', function() {
+		var $this = $(this),
+			$selections = $('.js-element-row.sel');
+
+		if (! $this.hasClass(isDisabled)) {
+			$selections.each(function(e, el) {
+				var $el = $(el),
+					newElement = $el.find($element)
+						.clone()
+						.addClass(isRemovable + ' fresh')
+						.prepend('<input name="fields[' + $el.data('name') + '][]" type="hidden" value="' + $el.data('id') + '">' +
+							'<a class="delete icon js-remove-element"></a>'
+						);
+
+				$elements.append(newElement);
+
+				$sorter.addItems(newElement);
+			});
+
+			$selections.removeClass(isSelected).addClass(isDisabled);
+
+			$this.addClass(isDisabled);
+
+			$garnishModal.hide();
+		}
+	});
+}
+
+/**
+ * Sort elements
+ */
+function dragSort() {
+	$sorter = new Garnish.DragSort($('.js-element.removable'));
+}
+
+/**
+ * Instantiate functions
+ */
+modal();
+removeElement();
+dragSort();
