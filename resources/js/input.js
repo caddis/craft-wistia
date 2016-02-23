4294a1ae6bc28b99c74ba0d072f4207f881ecba3
Ryan Payne <ryan@caddis.co>
2016-02-23 17:45:08 -0600

    Finalize new Wistia js setup

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,32 +1,75 @@
 (function($){
-	Wistia = {
-		/**
-		 * Init element selector
-		 */
-		init: function(settings) {
-			this.settings = settings;
-			this.elementSelectContainer = $('#' + this.settings.id);
-			this.openModalBtn = this.elementSelectContainer.find('.add');
-			this.isDisabled = 'disabled';
 
-			this.elements = new Craft.BaseElementSelectInput({
-				id: this.settings.id,
-				limit: this.settings.max,
-				onRemoveElements: function() {
-					updateAddBtn();
+Wistia = {};
+
+Wistia.Videos = Garnish.Base.extend({
+	/**
+	 * Init element selector
+	 */
+	init: function(settings) {
+		var scope = this;
+
+		scope.settings = settings;
+		scope.settings.max = parseInt(scope.settings.max);
+		scope.elementSelectContainer = $('#' + scope.settings.id);
+		scope.$addElementBtn = scope.elementSelectContainer.find('.add');
+		scope.isDisabled = 'disabled';
+
+		scope.elements = new Craft.BaseElementSelectInput({
+			id: scope.settings.id,
+			limit: scope.settings.max,
+			onRemoveElements: function() {
+				scope.updateAddBtn();
+			}
+		});
+
+		scope.updateAddBtn();
+
+		if (scope.$addElementBtn && scope.settings.max === 1) {
+			scope.$addElementBtn
+				.css('position', 'absolute')
+				.css('top', 0)
+				.css(Craft.left, 0);
+		}
+
+		scope.toggleModal();
+	},
+
+	/**
+	 * Update add video button
+	 */
+	updateAddBtn: function() {
+		if (this.elements.canAddMoreElements()) {
+			this.$addElementBtn.removeClass(this.isDisabled);
+
+			if (this.settings.max === 1) {
+				if (this.elements._initialized) {
+					this.$addElementBtn.velocity('fadeIn', Craft.BaseElementSelectInput.REMOVE_FX_DURATION);
+				} else {
+					this.$addElementBtn.show();
 				}
-			});
+			}
+		} else {
+			this.$addElementBtn.addClass(this.isDisabled);
 
-			this.toggleModal();
-		},
+			if (this.settings.max === 1) {
+				if (this.elements._initialized) {
+					this.$addElementBtn.velocity('fadeOut', Craft.BaseElementSelectInput.ADD_FX_DURATION);
+				} else {
+					this.$addElementBtn.hide();
+				}
+			}
+		}
+	},
 
-		/**
-		 * Open and close modal
-		 */
-		toggleModal: function() {
-			var scope = this;
+	/**
+	 * Open and close modal
+	 */
+	toggleModal: function() {
+		var scope = this;
 
-			scope.openModalBtn.filter(':not(.' + scope.isDisabled + ')').on('click', function() {
+		scope.$addElementBtn.on('click', function() {
+			if (! $(this).hasClass(scope.isDisabled)) {
 				if (! scope.modal) {
 					$.get(Craft.getActionUrl('wistia/videos/getModal', {projectIds: scope.settings.projectIds}), function(data) {
 						scope.modal = new Garnish.Modal($(data)[0]);
@@ -49,113 +92,99 @@
 
 						scope.searchElements();
 
-						// updateSelectBtn();
+						scope.updateSelectBtn();
 
-						// submitSelections();
+						scope.submitSelections();
 					});
 				} else {
 					scope.modal.show();
 
 					scope.updateSelectableElements();
 				}
-			});
-		},
-
-		/**
-		 * Update which modal elements are selectable
-		 */
-		updateSelectableElements: function() {
-			var scope = this;
-
-			scope.$elementRow.each(function(e, el) {
-				var $el = $(el);
-
-				$el.removeClass(scope.isDisabled);
-
-				$(scope.elements.getSelectedElementIds()).each(function(i, val) {
-					if ($el.data('id') === val) {
-						$el.addClass(scope.isDisabled);
-					}
-				});
-			});
-		},
-
-		/**
-		 * Update select button based on which modal elements are selected
-		 */
-		updateSelectBtn: function() {
-			if (this.selector.getTotalSelected() > 0) {
-				this.$modalSubmit.removeClass(this.isDisabled);
-			} else {
-				this.$modalSubmit.addClass(this.isDisabled);
 			}
-		},
-
-		/**
-		 * Search through elements
-		 */
-		searchElements: function() {
-			var scope = this;
-
-			scope.modal.$container.find('.search input').on('keyup', function() {
-				var $this = $(this);
-
-				// Clear the timer if one is set
-				if (scope.filterSearchTimer) {
-					clearTimeout(scope.filterSearchTimer);
-				}
-
-				scope.filterSearchTimer = setTimeout(function() {
-					// Retrieve the input field text
-					var filter = $this.val();
-
-					// Loop through the rows
-					scope.$elementRow.each(function() {
-						var $this = $(this);
-
-						// If the row does not contain the text phrase hide it
-						if ($this.data('title').search(new RegExp(filter, 'i')) < 0) {
-							$this.hide();
-						} else {
-							// Show the row if the phrase matches
-							$this.show();
-						}
-					});
-				}, 300);
-			});
-		}
-	};
+		});
+	},
 
 	/**
-	 * Update add video button
+	 * Update which modal elements are selectable
 	 */
-	function updateAddBtn() {
-		var $add = $('.js-add');
+	updateSelectableElements: function() {
+		var scope = this;
 
-		// Hide add more elements button if element max is met
-		if ($(scope.elements.getSelectedElementIds()).length >= $('.js-element-select').data('max')) {
-			$add.addClass(scope.isDisabled);
+		scope.$elementRow.each(function(e, el) {
+			var $el = $(el);
+
+			$el.removeClass(scope.isDisabled);
+
+			$(scope.elements.getSelectedElementIds()).each(function(i, val) {
+				if ($el.data('id') === val) {
+					$el.addClass(scope.isDisabled);
+				}
+			});
+		});
+	},
+
+	/**
+	 * Update select button based on which modal elements are selected
+	 */
+	updateSelectBtn: function() {
+		if (this.selector.getTotalSelected() > 0) {
+			this.$modalSubmit.removeClass(this.isDisabled);
 		} else {
-			$add.removeClass(scope.isDisabled);
+			this.$modalSubmit.addClass(this.isDisabled);
 		}
-	}
+	},
+
+	/**
+	 * Search through elements
+	 */
+	searchElements: function() {
+		var scope = this;
+
+		scope.modal.$container.find('.search input').on('keyup', function() {
+			var $this = $(this);
+
+			// Clear the timer if one is set
+			if (scope.filterSearchTimer) {
+				clearTimeout(scope.filterSearchTimer);
+			}
+
+			scope.filterSearchTimer = setTimeout(function() {
+				// Retrieve the input field text
+				var filter = $this.val();
+
+				// Loop through the rows
+				scope.$elementRow.each(function() {
+					var $this = $(this);
+
+					// If the row does not contain the text phrase hide it
+					if ($this.data('title').search(new RegExp(filter, 'i')) < 0) {
+						$this.hide();
+					} else {
+						// Show the row if the phrase matches
+						$this.show();
+					}
+				});
+			}, 300);
+		});
+	},
 
 	/**
 	 * Add selected modal elements to final list
 	 */
-	function submitSelections() {
-		$('.js-submit').on('click', function() {
-			var $this = $(this),
-				$selector = scope.selector,
-				$items = $selector.getSelectedItems();
+	submitSelections: function() {
+		var scope = this;
+
+		scope.$modalSubmit.on('click', function() {
+			var $this = $(this);
 
 			if (! $this.hasClass(scope.isDisabled)) {
-				$items.each(function(e, el) {
+				scope.selector.getSelectedItems().each(function(e, el) {
 					var $el = $(el),
 						newElement = $el.find($('.element'))
 							.clone()
 							.addClass('removable')
-							.prepend('<input name="fields[' + $('.js-element-select').data('name') + '][]" type="hidden" value="' + $el.data('id') + '">' +
+							.prepend('<input name="' + scope.settings.name + '[]" type="hidden" value="' + $el.data('id') + '">' +
 								'<a class="delete icon" title="'+Craft.t('Remove')+'"></a>');
 
 					// Add new elements to selection list
@@ -164,7 +193,7 @@
 					// Update elements object
 					scope.elements.addElements(newElement);
 
-					updateAddBtn();
+					scope.updateAddBtn();
 				});
 
 				// Hide the modal
@@ -174,8 +203,10 @@
 				$this.addClass(scope.isDisabled);
 
 				// Remove selected class from all items
-				$selector.deselectAll($items);
+				scope.selector.deselectAll(scope.selector.getSelectedItems());
 			}
 		});
 	}
+});
+
 })(jQuery);
