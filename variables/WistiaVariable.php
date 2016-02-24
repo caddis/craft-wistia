967d4ed5ed4d8657548e7cd8c8630dcc8f424c48
Ryan Payne <ryan@caddis.co>
2016-02-24 17:39:07 -0600

    Add thumbnail variable

--- variables/WistiaVariable.php
+++ variables/WistiaVariable.php
@@ -7,4 +7,10 @@ class WistiaVariable
 	{
 		return craft()->wistia_videos->getVideosByHashedId($params);
 	}
+
+	public function thumbnail($hashedId, $width = '', $height = '')
+	{
+		// TODO: just doing some initial testing
+		return $hashedId;
+	}
 }
